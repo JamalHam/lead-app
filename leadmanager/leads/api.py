ab2c2b748a97413b633f02449bc9a495b12556bd
Jamal Hamilton <hamilton.jamal.anthony@gmail.com>
2020-07-07 10:32:06 -0400

    registered knox and started accounts app

--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -5,6 +5,15 @@ from .serializers import LeadSerializer
 class LeadViewSet(viewsets.ModelViewSet):
     queryset=Lead.objects.all()
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated
     ]
+
+    serializer_class = LeadSerializer
+
+    def get_queryset(self):
+        return self.request.user.leads.all()
+
+    def perform_create(self, serializer):
+        serialize.save(owner=self.request.user)
+
     serializer_class = LeadSerializer
